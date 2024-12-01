2ec54e5ee1b36e2c8009ded572d3835507e8d009
Jaxydog <32988901+Jaxydog@users.noreply.github.com>
2024-12-01 03:49:23 -0500

    Initial commit, day 1 solution completed

---
+++ src/day_1.rs
@@ -0,0 +1,57 @@
+use std::io::{BufRead, BufReader};
+
+use anyhow::Result;
+
+#[allow(clippy::type_complexity)]
+fn parse_input() -> Result<(Box<[u32]>, Box<[u32]>)> {
+    let file = std::fs::File::open("data/day_1.txt")?;
+    let iterator = BufReader::new(file).lines();
+
+    let capacity = iterator.size_hint().1.unwrap_or_else(|| iterator.size_hint().0);
+    let mut lhs_array = Vec::<u32>::with_capacity(capacity);
+    let mut rhs_array = Vec::<u32>::with_capacity(capacity);
+
+    for result in iterator {
+        let line = result?;
+        let (lhs_str, rhs_str) = line.split_once("   ").unwrap();
+
+        lhs_array.push(lhs_str.parse()?);
+        rhs_array.push(rhs_str.parse()?);
+    }
+
+    Ok((lhs_array.into_boxed_slice(), rhs_array.into_boxed_slice()))
+}
+
+pub fn part_1() -> Result<()> {
+    let (mut lhs_array, mut rhs_array) = self::parse_input()?;
+    let mut differences = Vec::with_capacity(lhs_array.len());
+
+    lhs_array.sort_unstable();
+    rhs_array.sort_unstable();
+
+    for (index, lhs_value) in lhs_array.into_iter().enumerate() {
+        let rhs_value = rhs_array[index];
+        let difference = lhs_value.abs_diff(rhs_value);
+
+        differences.push(difference as u64);
+    }
+
+    println!("{}", differences.into_iter().sum::<u64>());
+
+    Ok(())
+}
+
+pub fn part_2() -> Result<()> {
+    let (lhs_array, rhs_array) = self::parse_input()?;
+    let mut multiples = Vec::with_capacity(lhs_array.len());
+
+    for lhs_value in lhs_array {
+        let rhs_appearances = rhs_array.iter().filter(|v| **v == lhs_value).count();
+
+        multiples.push(lhs_value as u64 * rhs_appearances as u64);
+    }
+
+    println!("{}", multiples.into_iter().sum::<u64>());
+
+    Ok(())
+}
---
+++ src/main.rs
@@ -0,0 +1,29 @@
+use anyhow::{Result, bail};
+
+mod day_1;
+
+fn main() -> Result<()> {
+    let Some(day) = std::env::args().nth(1) else {
+        bail!("missing day number");
+    };
+    let Some(part) = std::env::args().nth(2) else {
+        bail!("missing part number");
+    };
+
+    macro_rules! match_days {
+        ($($day:literal => $module:ident,)+) => {
+            match (day.parse::<u8>()?, part.parse::<u8>()?) {
+                $(
+                    ($day, 1) => self::$module::part_1(),
+                    ($day, 2) => self::$module::part_2(),
+                )+
+                (_, n) if n != 1 && n != 2 => bail!("expected 1 or 2 for part number"),
+                (..) => bail!("unknown or unfinished day number"),
+            }
+        };
+    }
+
+    match_days! {
+        1 => day_1,
+    }
+}
